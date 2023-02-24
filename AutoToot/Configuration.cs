6bef789bb71672ad4f9bd9364357c2e859362176
Tom O'Sullivan <tom@tomdotbat.dev>
2023-02-24 22:17:07 +0000

    refactor: update copyright year

--- AutoToot/Configuration.cs
+++ AutoToot/Configuration.cs
@@ -1,6 +1,6 @@
 ﻿/*
     COPYRIGHT NOTICE:
-	© 2022 Thomas O'Sullivan - All rights reserved.
+	© 2023 Thomas O'Sullivan - All rights reserved.
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
